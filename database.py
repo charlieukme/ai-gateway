0aca4b755b66d41f08512c455b92e32b5ea5349d
charlieukme <colombussep10@mail.com>
2026-03-18 14:23:39 +0530

    Fix database path for Vercel

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./ai_gateway.db"
+# Check if running on Vercel
+if os.environ.get('VERCEL'):
+    # Use /tmp directory on Vercel (writable but temporary)
+    SQLALCHEMY_DATABASE_URL = "sqlite:///tmp/ai_gateway.db"
+else:
+    # Local development
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./ai_gateway.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
